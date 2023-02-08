da175b634cd5568a34b6fbda39608322c505c74e
PavelTerekhovM <t89213064699@gmail.com>
2023-02-08 14:12:52 +0300

    create customized admin.py create models.py for admin create tests for testing admin

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-l@+xoht_e^m#nqui9pxlld_f%s4segojq57k@)3zek-z*%p5de
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+]
 
 
 # Application definition
@@ -127,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
