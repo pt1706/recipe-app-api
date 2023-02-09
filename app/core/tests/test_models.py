30a25f159c2908ad7a863e05efb6eefdb07bbab7
PavelTerekhovM <t89213064699@gmail.com>
2023-02-09 15:49:07 +0300

    Created new app recipes - ceated new model - created new serializers - created new view - created unit tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,8 @@
 """
 Test for models
 """
+from core import models
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -44,3 +46,18 @@ class ModelTest(TestCase):
         )
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
