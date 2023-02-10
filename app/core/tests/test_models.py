0388d5a0fce8aba55998a108dbe2c9f64998467e
PavelTerekhovM <t89213064699@gmail.com>
2023-02-10 16:31:16 +0300

    Created new model Tags - ceated model - created serializer - created view - added unit tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,11 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     """Test models"""
 
@@ -61,3 +66,10 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
